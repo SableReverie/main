adb583ef3bdb6a003aa0538770a8acc6aca2490a
Prince <princecultura10@gmail.com>
2026-05-27 22:26:59 +0800

    Create practice.cpp

---
+++ OneDrive/Desktop/C++Game/practice.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+
+int main() {
+   
+   // std::cout << "My name is Prince" << std::endl;
+   // std::cout << "My age is 19" << std::endl;
+   //std::cout << "I am a student at the University of Lagos";
+    return 0;
+}
